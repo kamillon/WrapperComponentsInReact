394f5feaf69ac2fdd76c0f918039c422d78a2368
kamilciecierski <kamil.ciecierski99@gmail.com>
2022-03-21 20:52:37 +0100

    first commit

---
+++ src/components/App/data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        name: 'Lion',
+        scientificName: 'Panthero leo',
+        size: 140,
+        diet: ['meat']
+    },
+    {
+        name: 'Gorilla',
+        scientificName: 'Gorilla beringei',
+        size: 205,
+        diet: ['plant', 'leaves']
+    },
+    {
+        name: 'Zebra',
+        scientificName: 'Equus quagga',
+        size: 322,
+        diet: ['plants']
+    }
+]
